3b35d2c763b50d28258d2c92fc4aa7c074510d8c
OksanaNagorniak <nagornyak.o.v.87@gmail.com>
2023-02-07 17:29:55 +0200

    hometask_13

---
+++ task_3.py
@@ -0,0 +1,12 @@
+# Напишите функцию sum_range(start, end), которая суммирует все целые числа от значения «start»
+# до величины «end» включительно. Если пользователь задаст первое число большее чем второе,
+# просто поменяйте их местами.
+
+
+def sum_range(start, end):
+    if start > end:
+        start, end = end, start
+        return sum(range(start, end + 1))
+
+
+print(sum_range(10, 7))
